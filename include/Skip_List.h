794d1fb40bc5f152d7cab1f8fa146dd40ba18e66
Ayato-ai <vanya.vladimir2016@yandex.ru>
2025-05-23 14:55:18 +0300

    add <limits>

--- include/Skip_List.h
+++ include/Skip_List.h
@@ -3,6 +3,7 @@
 // Noskov I.A.
 
 #pragma once
+#include<limits>
 #include<iostream>
 #include<list>
 #include<stdlib.h>
@@ -74,11 +75,11 @@ private:
 public:
     SkipList() = default;
     SkipList(std::size_t maxLevel) : maxLevel(maxLevel) {
-        head = new Node(std::INT_MIN);
+        head = new Node(INT_MIN);
         Node* temp = head;
 
         for (std::size_t i = 1; i < maxLevel; i++) {
-            temp->down = new Node(std::INT_MIN);
+            temp->down = new Node(INT_MIN);
             temp->down->up = temp;
             temp = temp->down;
         }
